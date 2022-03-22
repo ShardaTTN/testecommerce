c8829dabf6b56aadff5a633d042b81281a87214e
ShardaTTN <sharda.kumari@tothenew.com>
2022-03-22 13:46:14 +0530

    Made basic infrastructure for the project.

---
+++ src/main/java/com/tothenew/sharda/Ecommerce/TestEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.tothenew.sharda.Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestEcommerceApplication.class, args);
+	}
+
+}
